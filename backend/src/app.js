bd4551ca5f6f0015d0ac44da2156f85f673a687f
Ungratwar <ungratwaronkar@gmail.com>
2026-02-21 19:26:20 +0530

    Production ready build

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const analyzeRoutes = require("./routes/analyzeRoutes");
 
@@ -9,7 +10,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
+// API Routes
 app.use("/api", analyzeRoutes);
+
+// Serve React build
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 module.exports = app;

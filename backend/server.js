f14f87e48a1e790503fa49cb705c59efc0a1b81a
Ungratwar <ungratwaronkar@gmail.com>
2026-02-21 19:45:20 +0530

    Fixed production PORT config

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,7 @@
-require("dotenv").config();
 const app = require("./src/app");
-const path = require("path");
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, "build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });

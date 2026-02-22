81ff083ebdacdd80626fad23456307e844d7dc3d
Ungratwar <109030392+Ungratwar@users.noreply.github.com>
2026-02-22 11:17:44 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,13 +13,4 @@ app.use(express.json());
 // API routes
 app.use("/api", analyzeRoutes);
 
-// Serve React build
-app.use(express.static(path.join(__dirname, "../../my-react-app/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "../../my-react-app/build", "index.html")
-  );
-});
-
 module.exports = app;
